81918acb0d94e1ba7017054de844dc4958ee92d1
Nton23 <huutonau@gmail.com>
2023-05-18 08:13:46 -0700

    Add font style and spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,13 @@ module.exports = {
         grayishViolet: 'hsl(257, 7%, 63%)',
         veryDarkBlue: 'hsl(255, 11%, 22%)',
         veryDarkViolet: 'hsl(260, 8%, 14%)',
-      }
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      spacing: {
+        180: '32rem',
+      },
     },
   },
   plugins: [],
